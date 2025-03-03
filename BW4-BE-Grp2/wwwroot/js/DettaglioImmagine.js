c9e269c6d3c9d7f4fa946b7c49f3a3f58fb0198f
WalterX95 <walter_1995@hotmail.it>
2025-03-03 16:33:45 +0100

    Sezione Pagina Dettaglio

---
+++ BW4-BE-Grp2/wwwroot/js/DettaglioImmagine.js
@@ -0,0 +1,12 @@
+﻿/*Click Immagine Dettaglio 
+  Dev: WalterX95
+*/
+
+let mainProductImage = document.getElementById("mainProductImage");
+let carouselImage = document.getElementsByClassName("carouselImage");
+
+    for (let i = 0; i <= carouselImage.length; i++) {
+        carouselImage[i].addEventListener("click", function () {
+            mainProductImage.src = carouselImage[i].src;
+        });
+    }
