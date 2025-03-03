16b09d962ae5603737914706e6533bdec3c72fb7
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-03-03 10:58:56 +0100

    aggiunti controller

---
+++ BW4-BE-Grp2/Controllers/CarrelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BW4_BE_Grp2.Controllers
+{
+    public class CarrelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BW4-BE-Grp2/Controllers/DettaglioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BW4_BE_Grp2.Controllers
+{
+    public class DettaglioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
