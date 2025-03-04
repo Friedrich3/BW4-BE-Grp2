3c924318317163c8eafaaa5e223afad755910892
Inchino <alessandro.incalza22@gmail.com>
2025-03-04 11:45:07 +0100

    Creazione Admin View e correzione footer

--- BW4-BE-Grp2/Controllers/AdminController.cs
+++ BW4-BE-Grp2/Controllers/AdminController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using BW4_BE_Grp2.Models;
 
 namespace BW4_BE_Grp2.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly string _connectionString;
+
+        public AdminController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Prodotto> prodotti = new List<Prodotto>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "SELECT IdProdotto, Nome, Prezzo, Immagine, Brand FROM Prodotti";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        prodotti.Add(new Prodotto
+                        {
+                            IdProdotto = reader.GetGuid(0),
+                            Nome = reader.GetString(1),
+                            Prezzo = reader.GetDecimal(2),
+                            Immagine = reader.GetString(3),
+                            Brand = reader.GetString(4)
+                        });
+                    }
+                }
+            }
+
+            return View(prodotti);
         }
     }
 }
