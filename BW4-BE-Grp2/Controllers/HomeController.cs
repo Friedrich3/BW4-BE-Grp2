750a6587e00b7fbb3dc04e834a9772ebf350e6e4
vintry <oennabati3@gmail.com>
2025-03-04 12:51:08 +0100

    commit 2

--- BW4-BE-Grp2/Controllers/HomeController.cs
+++ BW4-BE-Grp2/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
 using System.Diagnostics;
 using BW4_BE_Grp2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace BW4_BE_Grp2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string _connectionString;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Categoria> categorie = new List<Categoria>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "SELECT IdCategoria, NomeCategoria, ImmagineCategoria FROM Categoria";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        categorie.Add(new Categoria
+                        {
+                            IdCategoria = reader.GetInt32(0),
+                            NomeCategoria = reader.GetString(1),
+                            ImmagineCategoria = reader.GetString(2)
+                        });
+                    }
+                }
+            }
+
+            return View(categorie);
         }
 
         public IActionResult Privacy()
---
+++ BW4-BE-Grp2/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace BW4_BE_Grp2.Models
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string NomeCategoria { get; set; }
+        public string ImmagineCategoria { get; set; }
+    }
+}
