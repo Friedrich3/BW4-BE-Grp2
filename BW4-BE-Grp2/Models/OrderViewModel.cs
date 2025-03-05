5d70784c6de70e5a3b9a7a4c51e0e831c35c5019
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-03-05 15:39:59 +0100

    bottone dettaglio aggiunta ordine

---
+++ BW4-BE-Grp2/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BW4_BE_Grp2.Models
+{
+    public class OrderViewModel
+    {
+        public Guid IdProdotto { get; set; }
+        public decimal PrezzoUnita { get; set; }
+        public int Quantita { get; set; }
+        public Guid IdCarrello { get; set; }
+
+    }
+}
